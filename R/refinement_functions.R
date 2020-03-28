1aefb2d82b0da134611d67b459e36ac0a0675cb9
JDO <atharos@vivaldi.net>
2020-03-28 17:52:30 +0000

    Exapanding the refinement function to handle the incorrect years in some of the data

--- R/refinement_functions.R
+++ R/refinement_functions.R
@@ -17,8 +17,16 @@
 csr_PurRefineOutputData <- function(input_ds){
         input_ds %>% 
         mutate(inferred.date = csr_PurDateParseFromFileName(Source.url)) %>% 
+                # Infilling the missing dates
         mutate(Payment.date.infill = coalesce(Payment.date, inferred.date)) %>% 
         mutate(infilled.date.flag = is.na(Payment.date) & !is.na(inferred.date)) %>% 
+                # Handling the incorrect year data
+        mutate(Payment.date.infill = case_when(
+                lubridate::year(Payment.date.infill) < "2010" 
+                |  lubridate::year(Payment.date.infill) >  lubridate::year(Sys.Date()) 
+                ~ lubridate::`year<-`(Payment.date.infill, lubridate::year(inferred.date))
+                , T ~ Payment.date.infill
+        )) %>% 
         select(-inferred.date, -Payment.date) %>% 
         rename(Payment.date = Payment.date.infill)
 } 
